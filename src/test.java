9b5146496102b3dd800d274b959fbbc537b22f0c
mixmind <mixmind@notebook>
2018-01-01 19:41:44 +0200

    Fix

--- src/test.java
+++ src/test.java
@@ -15,9 +15,9 @@ public class test {
 		// TODO Auto-generated method stub
 // toCSV k1=new toCSV("d:\\dd\\");
 	//	Network test=csvBase.readCSV("d:\\data\\");
-	Algo.routerPlaceAlgo1("c:\\Users\\mixmind\\eclipse-workspace\\OOP matala\\files\\input\\Ex2\\_comb_all_BM2_.csv");
+	//Algo.routerPlaceAlgo1("c:\\Users\\mixmind\\eclipse-workspace\\OOP matala\\files\\input\\Ex2\\_comb_all_BM2_.csv");
 	//Algo2.clientPlaceAlgo2("c:\\Users\\mixmind\\eclipse-workspace\\OOP matala\\files\\input\\Ex2\\_comb_all_BM2_.csv","c:\\Users\\mixmind\\eclipse-workspace\\OOP matala\\files\\input\\Ex2\\_comb_no_gps_ts1.csv");
-		//toKML a = new toKML("d://dd//2017-12-21 10.45.20.csv");
+		toKML a = new toKML("d://dd//2017-12-21 10.45.20.csv");
 	}
 	
 	
